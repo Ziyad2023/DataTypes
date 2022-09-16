7d470339d12532be7af15ec42d844eed4a9bf26e
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-09-16 11:26:29 -0700

    Done

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,9 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i;
+        float f = 2.5f;
+        boolean flag = true;
+        System.out.println(f);
     }
 }
